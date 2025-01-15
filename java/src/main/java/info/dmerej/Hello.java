b85470af6e832b2bb439ae9274f5f3c78f8e8625
Dieuveille MPOUNKOUO <94381407+DieuveilleSuthi@users.noreply.github.com>
2025-01-15 10:59:01 +0100

    Feature test

--- java/src/main/java/info/dmerej/Hello.java
+++ java/src/main/java/info/dmerej/Hello.java
@@ -1,7 +1,46 @@
 package info.dmerej;
 
-public class Hello {
-    public int getAnswer() {
-        return 42;
+class Rover {
+    private int x;
+    private int y;
+    private String direction;
+
+
+    public Rover(int x, int y, String direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+    public String getFacing(){
+        return direction;
+    }
+    public void changeDirection(String newDirection) {
+        this.direction = newDirection;
+    }
+
+    public void moveForward() {
+        if (direction.equals("N")) {
+            y += 1;
+        }
+         if (direction.equals("O")) {
+            X += -1;
+        }
+         if (direction.equals("W")) {
+            X += 1;
+        }
+         if (direction.equals("S")) {
+            y += -1;
+        }
     }
 }
+
--- java/src/test/java/info/dmerej/HelloTest.java
+++ java/src/test/java/info/dmerej/HelloTest.java
@@ -4,13 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HelloTest {
+public class RoverTest {
+
 
     @Test
-    void it_works() {
-        var hello = new Hello();
-        var actual = hello.getAnswer();
-        assertEquals(42, actual);
+    void testMoveForwardFacingNorth(){
+        Rover rover = new Rover(0,0,"N");
+        rover.moveForward();
+        assertEquals(0, rover.getX());
+        assertEquals(1, rover.getY());
+        assertEquals("N", rover.getFacing());
+
+        Rover rover2= new Rover(0,0,"W");
+        rover.moveForward();
+        assertEquals(1, rover.getX());
+        assertEquals(1, rover.getY());
+        assertEquals("N", rover.getFacing());
+        
+
 
     }
 }
